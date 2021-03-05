5d41c20e5b4216799280a60957643e50e0cec0e4
kartik Patidar <kartikpatidar815@gmail.com>
2021-03-05 16:32:09 +0530

    api and form initials

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  let baseURL = "https://kitsu.io/api/edge";
+  let myURL = `${baseURL}/anime?filter[text]=${searchQuery}`;
+
+  // useEffect(() => {
+  //   axios.get(myURL, {
+  //     headers: {
+  //       'Accept': 'application/vnd.api+json',
+  //       'Content-Type': 'application/vnd.api+json'
+  //     }
+  //   }).then((response) => console.log(response))
+  // }, [myURL])
+
+  function handelChange(e) {
+    setQuery(e.target.value);
+  }
+
+  function handelSubmit(e) {
+    e.preventDefault();
+    setSearchQuery(query);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handelSubmit.bind(this)}>
+        <input type="text" onChange={handelChange.bind(this)} />
+        <button>Search</button>
+      </form>
+      <br />
+      {searchQuery}
     </div>
   );
 }
